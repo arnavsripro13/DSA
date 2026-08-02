7b78c5ec17f422e6e07ad402ee2e1f1ef585910e
arnavsripro13 <qwertyarnav0808@gmail.com>
2026-08-02 22:22:14 +0530

    2nd program

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter Number 1: ");
+    scanf("%d",&a);
+    printf("Enter Number 2: ");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("Enter sum: %d",sum);
+    return 0;
+}
