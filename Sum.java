73e896309345e49c6a18d50793c39ae2396a5fc1
arnavsripro13 <qwertyarnav0808@gmail.com>
2026-08-02 22:56:16 +0530

    2nd Program

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sum
+{
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter Number 1 : ");
+        a=sc.nextInt();
+        System.out.println("Enter Number 2: ");
+        b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The sum is = "+sum);
+
+    }
+}
